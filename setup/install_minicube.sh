d60b5bd4ddbba1767a58aedfbdb10cfdded8a0f0
GavrilS <gavr1lll@abv.bg>
2025-08-16 18:56:57 +0300

    Added scripts to install virtualbox and kubectl

---
+++ setup/install_minicube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "This installation method is for Ubuntu..."
+
+echo "Minucube is a tool that allows you to run Kubernetes locally for learning and testing. It has a few dependencies that need to be installed with it - kubectl, a Hypervisor like VirtualBox and virtualization needs to be enabled on our machine."
+
+# Instal kubectl
+# echo "***Install kubectl"
+
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+# echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
