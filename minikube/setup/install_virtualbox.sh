70249b0fe2e136be6b06b7ec61f07d58b49d3b7a
GavrilS <gavr1lll@abv.bg>
2025-08-16 20:30:02 +0300

    Refactoring the project structure a bit

---
+++ minikube/setup/install_virtualbox.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "This script will install VirtualBox on Ubuntu"
+
+# Install VirtualBox
+sudo apt update
+sudo apt install virtualbox
+# sudo apt install virtualbox-ext-pack
+
+echo "To run virtualbox just run the command: `virtualbox`"
