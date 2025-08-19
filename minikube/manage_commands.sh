a20acada155a4bbb2c87f79747ea66b7cea7feb7
GavrilS <gavr1lll@abv.bg>
2025-08-19 22:12:14 +0300

    Adding commands to setup/manage the kubernetes cluster

---
+++ minikube/manage_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This script is only a guide on minikube commands"
+
+echo "To start your kubernetes cluster run:"
+echo "minikube start"
+
+echo "Use kubectl or minikube kubectl to interact with the cluster. Example:"
+echo "(minikube) kubectl get po -A"
+
+echo "To look at the Kubernetes Dashboard:"
+echo "minikube dashboard"
+
