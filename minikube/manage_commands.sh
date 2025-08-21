82b0c7a8b8eb111d8ec008bd10fbd4ef597decda
GavrilS <gavr1lll@abv.bg>
2025-08-21 22:34:38 +0300

    Adding kubernetes commands to the list of commands for the tutorial

--- minikube/manage_commands.sh
+++ minikube/manage_commands.sh
@@ -2,12 +2,76 @@
 
 echo "This script is only a guide on minikube commands"
 
-echo "To start your kubernetes cluster run:"
+echo "***GENERAL COMMANDS***"
+
 echo "minikube start"
+echo "  Set up and start a kubernetes cluster"
 
-echo "Use kubectl or minikube kubectl to interact with the cluster. Example:"
 echo "(minikube) kubectl get po -A"
+echo "  Use kubectl or minikube kubectl to interact with the cluster"
 
-echo "To look at the Kubernetes Dashboard:"
 echo "minikube dashboard"
+echo "  Look at the Kubernetes Dashboard"
 
+echo "kubectl create -f <file_name>"
+echo "  Create resources defined in an yaml file"
+
+echo "kubectl destroy -f <file_name>"
+echo "  Destroy resources defined in an yaml file"
+
+echo "kubectl apply -f <file_name>"
+echo "  Apply changes to the configs of resources defined in an yaml file"
+
+echo "kubectl get all"
+echo "  Get all available resources in the cluster"
+
+echo "kubectl apply -f <file-name>"
+echo "  Apply changes to an yaml file to the resources created by the file"
+
+echo "***POD COMMANDS***"
+
+echo "kubectl get pods"
+echo "  Lists all available pods"
+
+echo "kubectl describe pod <pod-name>"
+echo "  List full information on the pod"
+
+echo "kubectl run <pod-name> --image=<image-name>"
+echo "  Run a pod with the specified name using the specified image"
+
+echo "kubectl delete pods <pod-name1> ... <pod-nameN>"
+echo "  Deletes the specified pods"
+
+echo "kubectl set image (-f <file-name>) container_name=image"
+echo "  Set a specific image to be used for the container"
+
+echo "***REPLICASET COMMANDS***"
+
+echo "kubectl get replicasets"
+echo "  List the available replicasets"
+
+echo "kubectl scale --replicas=6 -f <file-name>"
+echo "  Scale the number of replicas for a replicaset defined in an yaml file"
+
+echo "kubectl scale --replicas=6 replicaset <replicaset-name>"
+echo "  Scale the number of replicas for the specified replicaset"
+
+echo "kubectl delete replicasets <replicaset1> ... <replicasetN>"
+echo "  Deletes the specified replicasets"
+
+echo "kubectl replace -f <file-name>"
+echo "  Replaces the replicaset defined in an yaml file"
+
+echo "***DEPLOYMENT COMMANDS***"
+
+echo "kubectl get deployments"
+echo "  Lists the available deployments"
+
+echo "kubectl rollout status deployment/<deployment-name>"
+echo "  Shows the status of the deployment update"
+
+echo "kubectl rollout history deployment/<deployment-name>"
+echo "  Lists the revision history of the deployment"
+
+echo "kubectl rollout undo deployment/<deployment-name>"
+echo "  Revert the deployment to the previous revision"
