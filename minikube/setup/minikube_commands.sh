78a04c73f94feda6fb0245cb5e06744d9195faed
GavrilS <gavr1lll@abv.bg>
2025-08-26 21:44:08 +0300

    Added additional commands for setting up/managing minikube kubernetes clusters

--- minikube/setup/minikube_commands.sh
+++ minikube/setup/minikube_commands.sh
@@ -7,6 +7,14 @@ echo "minikube start"
 echo "  Starts a kubernetes cluster with the default driver - virtualbox"
 echo "*******************************************************"
 
+echo "minikube delete --all"
+echo "  Deletes a kubernetes cluster"
+echo "*******************************************************"
+
+echo "minikube stop"
+echo "  Stops a kubernetes cluster"
+echo "*******************************************************"
+
 echo "minikube start --alsologtostderr -v=1"
 echo "  Starts a kubernetes cluster with logs for extra debugging information"
 echo "*******************************************************"
@@ -16,7 +24,7 @@ echo "  Starts a kubernetes cluster with explicitly specifying the driver to use
 echo "*******************************************************"
 
 echo "minikube start --driver=docker"
-echo "  Starts a kubernetes cluster with docker as the driver"
+echo "  Starts a kubernetes cluster with docker as the driver; if the user doesn't have permissions to run docker it will require permission updates with the following command: `sudo usermod -aG docker $USER && newgrp docker`"
 echo "*******************************************************"
 
 echo "minikube config set driver docker"
