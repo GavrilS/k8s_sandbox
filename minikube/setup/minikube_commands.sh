2fb1dc8ab52471f4f931fb0a98b6f45201501dac
GavrilS <gavr1lll@abv.bg>
2025-08-26 08:36:47 +0300

    Added some specific minikube commands

---
+++ minikube/setup/minikube_commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "This script is for showing available `minikube` commands"
+echo "*******************************************************"
+
+echo "minikube start"
+echo "  Starts a kubernetes cluster with the default driver - virtualbox"
+echo "*******************************************************"
+
+echo "minikube start --driver=virtualbox"
+echo "  Starts a kubernetes cluster with explicitly specifying the driver to use - virtualbox"
+echo "*******************************************************"
+
+echo "minikube start --driver=docker"
+echo "  Starts a kubernetes cluster with docker as the driver"
+echo "*******************************************************"
+
+echo "minikube config set driver docker"
+echo "  Change the default driver to docker"
+echo "*******************************************************"
+
+echo "minikube dashboard"
+echo "  Show a kubernetes dashboard"
+echo "*******************************************************"
