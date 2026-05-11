430f488352ac7daa0dbd2a4b30718deadf67f438
GavrilS <gavr1lll@abv.bg>
2026-05-11 07:38:48 +0300

    Added an additional command example

--- k8s_labs/deployments/deployments.sh
+++ k8s_labs/deployments/deployments.sh
@@ -7,8 +7,8 @@ echo "Get more information on a deployment"
 echo "  kubectl describe deployment <deployment_name>"
 echo "  kubectl get deployments -o wide"
 
-echo "Create a deployment from a file"
-echo "  kubectl apply -f <file_name>"
+echo "Create a deployment from command line"
+echo "  kubectl create deployment <deployment-name> --image=<image-name> --replicas=<num-replicas>"
 
 echo "Create a deployment from a file called 'deployment.yaml'"
 echo "  kubectl apply -f deployment.yaml"
