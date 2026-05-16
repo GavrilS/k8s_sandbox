89756635ed92e54e8ac88afdf56c35e0d83de87d
GavrilS <gavr1lll@abv.bg>
2026-05-16 13:39:10 +0300

    Added lab questions on static pods

---
+++ k8s_labs/scheduler/static_pods.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "In which directory can we find the manifest files for static pods?"
+echo "  /etc/kubernetes/manifests"
+
+echo "Create a static pod named 'static-busybox', using the 'busybox' image, run in the 'default' namespace and the command 'sleep 1000'"
+echo "  To make this you need to create the pod manifest file and put it in the directory where the static pod manifest files live. An example definition file is './static_pod.yaml'."
+
+echo "Delete a static pod called 'static-greenbox'."
+echo "  Steps:"
+echo "   1. Locate the node the pod was created on:"
+echo "      kubectl get pods -A -o wide"
+echo "   2. Log in to the node:"
+echo "      ssh user@node_name"
+echo "   3. Find the static pod definitions file in the kubelet static pod manifest directory(usually /etc/kubernetes/manifests/). Delete the file and this will remove the pod."
+echo "   4. If the file was in the default location then you don't need to do anything else. If the file was not there, this means the 'kubelet' was run with a flag that changes the default location for static pod manifest files. To find the new location run the following:"
+echo "      ps aux | grep kubelet | grep -- --config"
+echo "      The config file will appear in this format: --config=/config_file.yaml. Then run the following command:"
+echo "      sudo grep staticPodPath config_file.yaml"
+echo "   5. Locate the file in the path provided by the above command and delete it."
+
